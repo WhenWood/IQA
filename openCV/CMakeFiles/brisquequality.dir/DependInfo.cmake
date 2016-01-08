b5da68d2ed2b7e3f13e70b84dec025c7991b14b4
whenwood <whenwood@CattaiL.local>
2016-01-08 21:56:46 +0800

    brisque

---
+++ openCV/CMakeFiles/brisquequality.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/whenwood/Desktop/brisque/brisque/brisque.cpp" "/Users/whenwood/Desktop/brisque/brisque/CMakeFiles/brisquequality.dir/brisque.cpp.o"
+  "/Users/whenwood/Desktop/brisque/brisque/computescore.cpp" "/Users/whenwood/Desktop/brisque/brisque/CMakeFiles/brisquequality.dir/computescore.cpp.o"
+  "/Users/whenwood/Desktop/brisque/brisque/libsvm/svm.cpp" "/Users/whenwood/Desktop/brisque/brisque/CMakeFiles/brisquequality.dir/libsvm/svm.cpp.o"
+  "/Users/whenwood/Desktop/brisque/brisque/main.cpp" "/Users/whenwood/Desktop/brisque/brisque/CMakeFiles/brisquequality.dir/main.cpp.o"
+  "/Users/whenwood/Desktop/brisque/brisque/trainModel.cpp" "/Users/whenwood/Desktop/brisque/brisque/CMakeFiles/brisquequality.dir/trainModel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "GCC_COMPILER=1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ openCV/main.cp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  brisque
+//
+//  Created by whenwood on 16/1/8.
+//  Copyright (c) 2016年 whenwood. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
